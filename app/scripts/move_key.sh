c70b8ba0c46c44daf12ccf59f06ad5214049216f
James Bednell <james@radixdlt.com>
2019-11-11 10:17:30 +0000

    improve move_key.sh for replacing keys

--- app/scripts/move_key.sh
+++ app/scripts/move_key.sh
@@ -11,18 +11,18 @@ send "key 1 \r"
 expect  "gpg> "
 
 send "keytocard \r"
+expect "Your selection? "
+send "2 \r"
 expect { 
-    "Your selection? " { 
-        send "2 \r"
-        exp_continue
-    }
     "Replace existing key? (y/N) " {
         send "y \r"
+        exp_continue
+    }
+    "gpg>" {
+        send "key 1 \r"
     }
 }
 
-expect  "gpg>"
-send "key 1 \r"
 expect  "gpg> "
 
 
@@ -31,18 +31,18 @@ send "key 2 \r"
 expect  "gpg> "
 
 send "keytocard \r"
+expect "Your selection? "
+send "1 \r"
 expect { 
-    "Your selection? " { 
-        send "1 \r"
-        exp_continue
-    }
     "Replace existing key? (y/N) " {
         send "y \r"
+        exp_continue
+    }
+    "gpg>" {
+        send "key 2 \r"
     }
 }
 
-expect "gpg>"
-send "key 2 \r"
 expect "gpg> "
 
 
@@ -51,18 +51,18 @@ send "key 3 \r"
 expect "gpg> "
 
 send "keytocard \r"
+expect "Your selection? "
+send "3 \r"
 expect { 
-    "Your selection? " { 
-        send "3 \r"
-        exp_continue
-    }
     "Replace existing key? (y/N) " {
         send "y \r"
+        exp_continue
+    }
+    "gpg>" {
+        send "key 3 \r"
     }
 }
-expect "gpg> "
 
-send "key 3 \r"
 expect "gpg> "
 
 # save
