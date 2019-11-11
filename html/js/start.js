5fef7d7040c3bece31c11acd0b2b154c83bf0d02
James Bednell <james@radixdlt.com>
2019-11-11 15:10:53 +0000

    add progressbar
    tick version

--- html/js/start.js
+++ html/js/start.js
@@ -1,7 +1,4 @@
 var slider;
-const notification = document.getElementById('notification');
-const message = document.getElementById('message');
-const restartButton = document.getElementById('restart-button');
 
 $(document).ready(function(){
 
@@ -10,33 +7,4 @@ $(document).ready(function(){
         infiniteLoop: false,
     });
  
-    // Get app version
-    ipcRenderer.send('app_version');
-    ipcRenderer.on('app_version', (event, arg) => {
-        ipcRenderer.removeAllListeners('app_version');
-        document.title = "Guide " + arg.version;
-    });
-    ipcRenderer.on('download_progress', (event, progress) => {
-        message.innerText = `Downloaded ${progress}%`;
-        notification.classList.remove('hidden');
-    })
-    
-    ipcRenderer.on('update_available', () => {
-        ipcRenderer.removeAllListeners('update_available');
-        message.innerText = 'A new update is available. Downloading now...';
-        notification.classList.remove('hidden');
-    });
-    ipcRenderer.on('update_downloaded', () => {
-        ipcRenderer.removeAllListeners('update_downloaded');
-        message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
-        restartButton.classList.remove('hidden');
-        notification.classList.remove('hidden');
-    });
-    
 });
-function closeNotification() {
-    notification.classList.add('hidden');
-}
-function restartApp() {
-    ipcRenderer.send('restart_app');
-}
---
+++ html/js/update.js
@@ -0,0 +1,90 @@
+const notification = document.getElementById('notification');
+const message = document.getElementById('message');
+const restartButton = document.getElementById('restart-button');
+var progressbar;
+
+$(document).ready(function(){
+
+
+    // Progress bar
+    initializeProgressbar();
+
+    // Get app version
+    ipcRenderer.send('app_version');
+    ipcRenderer.on('app_version', (event, arg) => {
+        ipcRenderer.removeAllListeners('app_version');
+        document.title = "Guide " + arg.version;
+    });
+    ipcRenderer.on('download_progress', (event, progress) => {
+        showProgressbar()
+        message.innerText = ``;
+        progressbar.animate(progress/100);
+        notification.classList.remove('hidden');
+    })
+    
+    ipcRenderer.on('update_available', () => {
+        ipcRenderer.removeAllListeners('update_available');
+        message.innerText = 'A new update has been detected. Downloading...';
+        notification.classList.remove('hidden');
+    });
+    ipcRenderer.on('update_downloaded', () => {
+        ipcRenderer.removeAllListeners('update_downloaded');
+        showProgressbar(false)
+        message.innerText = 'Update downloaded. It will be installed on restart. Would you like to restart?';
+        restartButton.classList.remove('hidden');
+        notification.classList.remove('hidden');
+    });
+
+
+
+    
+});
+
+function closeNotification() {
+    notification.classList.add('hidden');
+}
+
+function restartApp() {
+    ipcRenderer.send('restart_app');
+}
+
+function initializeProgressbar() {
+    progressbar = new ProgressBar.Line("#update_progressbar", {
+        strokeWidth: 4,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#78d378',
+        trailColor: '#eee',
+        trailWidth: 1,
+        svgStyle: {width: '100%', height: '100%'},
+        text: {
+            style: {
+                // Text color.
+                // Default: same as stroke color (options.color)
+                color: '#999',
+                position: 'absolute',
+                right: '0',
+                top: '30px',
+                padding: 0,
+                margin: 0,
+                transform: null
+            },
+            autoStyleContainer: false
+        },
+        from: {color: '#78d378'},
+        to: {color: '#78d378'},
+        step: (state, bar) => {
+            bar.setText(Math.round(bar.value() * 100) + ' %');
+        }
+    });
+}
+
+function showProgressbar(show) {
+    if(show == undefined)
+        show = true
+
+    if(show == true)
+        $("#update_progressbar").removeClass("hidden")
+    else
+        $("#update_progressbar").addClass("hidden")
+}
